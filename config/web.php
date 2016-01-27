574e483f6bbddb708ce4c00f2dcc20c5d306c426
triing <triing@snowstorm.dreamhost.com>
2016-01-27 12:22:06 -0800

    Dodana avtentikacija uporabnikov

--- config/web.php
+++ config/web.php
@@ -6,6 +6,15 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+	'modules' => [
+		'user' => [
+			'class' => 'dektrium\user\Module',
+			'enableUnconfirmedLogin' => true,
+			'confirmWithin' => 21600,
+			'cost' => 12,
+			'admins' => ['admin']
+		],
+	],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -14,19 +23,35 @@ $config = [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
+/*        'user' => [
+//            'identityClass' => 'app\models\User',
+//            'enableAutoLogin' => true,
+			'class' => 'dektrium\user\Module',
+			'enableUnconfirmedLogin' => true,
+			'confirmWithin' => 21600,
+			'cost' => 12,
+			'admins' => ['admin']
+        ],*/
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
         'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
+//            'class' => 'yii\swiftmailer\Mailer',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+//            'useFileTransport' => true,
+			'class' => 'yii\swiftmailer\Mailer',
+			'viewPath' => '@app/mailer',
+			'useFileTransport' => false,
+			'transport' => [
+				'class' => 'Swift_SmtpTransport',
+				'host' => 'smtp.gmail.com',
+				'username' => 'bind@triing.si',
+				'password' => 'P0veziMe!',
+				'port' => '587',
+				'encryption' => 'tls',
+            ],
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
